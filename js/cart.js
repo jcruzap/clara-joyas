d5c212c6b425966e2647f4198e22cb4e48c9ebe7
Michael Alvarez <michaelalvarezxd@gmail.com>
2022-08-23 07:30:11 -0300

    reeplicando la card de productos mostrando precio y cantidad solamente , añadi la libreria sweet alert

--- js/cart.js
+++ js/cart.js
@@ -35,18 +35,39 @@ let cartCounter = 0;
 
 // Haciendo uso de clases y metodo constructor para crear el carrito 
 class Cart {
-    constructor(cartId, name, description, source, price) {
+    constructor(cartId, name, description, source, price, amount) {
         this.cartId = cartId
         this.name = name
         this.description = description
         this.source = source
         this.price = price
+        this.amount = amount
     }
 }
 
 // Funcion flecha que recibe como parametro uno de los productos y lo transforma a una cadena
 const productsHTML = (product) => {
-    return `
+    // return `
+    //     <section class="producto">
+    //         <img src="../img/products/${product.source}" alt="foto de producto">
+    //         <div class="productoCard">
+    //             <div class="drow">
+    //                 <div class="productoTitulo">
+    //                     <h3>${product.name}</h3>
+    //                     <h4>$${product.price}</h4>
+    //                 </div>
+    //                 <div>
+    //                     <button class="botones comprar">Comprar</button>
+    //                 </div>
+    //             </div>
+    //             <hr>
+    //             <p>${product.description}</p>
+    //             <div class="productoBotones">
+    //                 <button id="product-btn-${product.id}" class="botones">Agregar al carrito</button>
+    //             </div>
+    //         </div>
+    //     </section>`
+        return `
         <section class="producto">
             <img src="../img/products/${product.source}" alt="foto de producto">
             <div class="productoCard">
@@ -71,27 +92,36 @@ const productsHTML = (product) => {
 
 // Funcion flecha que recibe como parametro uno de los productos del carrito y lo transforma a una cadena
 const cartHTML = (product) => {
+    // return `
+    //     <div class="card" style="width: 18rem;">
+    //         <img src="../img/products/${product.source}" class="card-img-top" alt="...">
+    //         <div class="card-body">
+    //             <h5 class="card-title">${product.name}</h5>
+    //             <p class="card-text">${product.description}</p>
+    //             <a href="#" class="btn btn-danger" id="delete-btn-${product.cartId}" >Eliminar</a>
+    //         </div>
+    //     </div>
+    // `
     return `
-        <div class="card" style="width: 18rem;">
-            <img src="../img/products/${product.source}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.description}</p>
-                <a href="#" class="btn btn-danger" id="product-btn-${product.cartId}" >Eliminar</a>
-            </div>
+    <div class="card" style="width: 18rem;">
+        <img src="../img/products/${product.source}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${product.name}</h5>
+            <p class="card-text">${product.amount} x <span>${product.price}</span></p>
         </div>
-    `
+    </div>
+`
 }
 
 // Añade los eventos a los botones del carrito de compras lateral para poder eliminarlos
 const buttonCart = () => {
     const counterSelector = document.getElementById('counter');
     cart.forEach(product => {
-        let cartButtonSelector = document.getElementById(`product-btn-${product.cartId}`);
-        cartButtonSelector.addEventListener("click", () =>{
+        let cartButtonSelector = document.getElementById(`delete-btn-${product.cartId}`);
+        cartButtonSelector.addEventListener("click", () => {
             const index = cart.findIndex((el) => el.cartId == product.cartId);
             cart.splice(index, 1);
-            cartCounter --;
+            cartCounter--;
             counterSelector.innerHTML = cartCounter;
             showCart();
         });
@@ -114,17 +144,36 @@ const showCart = () => {
 const buttonProducts = () => {
     const counterSelector = document.getElementById('counter');
     const cartIconSelector = document.getElementById('cartIcon');
-    
+    let amount = 1;
     products.forEach(product => {
         let buttonSelector = document.getElementById(`product-btn-${product.id}`);
         buttonSelector.addEventListener("click", () => {
+            // cartIconSelector.classList.remove("animate__shakeX");
             cartIconSelector.classList.add("animate__shakeX");
-            cart.push(new Cart(cartCounter, product.name, product.description, product.source, product.price));
+            cart.push(new Cart(
+                cartCounter,
+                product.name,
+                product.description,
+                product.source,
+                product.price,
+                amount
+            ));
             cartCounter++;
             counterSelector.innerHTML = cartCounter;
+            Swal.fire({
+                title: 'Producto añadido con éxito al carrito',
+                icon: 'success',
+                showCloseButton: true,
+                showCancelButton: true,
+                focusConfirm: false,
+                confirmButtonColor: '#98ab42',
+                cancelButtonColor: '#151D1B',
+                confirmButtonText: '<a href="#" class="mostrarCarrito"><i class="fas fa-shopping-cart"></i> Ver carrito</a>',
+                cancelButtonText: 'Seguir comprando',
+            });
             showCart();
         });
-        
+
     });
 }
 
