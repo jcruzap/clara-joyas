580810fd095cb33f3b5b7f29c0a34fd23846a765
Michael Alvarez <michaelalvarezxd@gmail.com>
2022-09-06 14:37:41 -0300

    Aplicando el envio de email a traves del formulario de contacto
    Utilizando la API REST de EmailJS se logra enviar un correo a una cuenta
    de gmail luego de completar los campos necesarios para dicho objetivo.

---
+++ js/contact.js
@@ -0,0 +1,60 @@
+const emailjs_data = {
+    SERVICE_ID: "service_nzwa1y5",
+    TEMPLATE_ID: "template_4xiqyc9",
+    PUBLIC_KEY: "PRk_FspI8BLnJ9cwl",
+    API_URL: 'https://api.emailjs.com/api/v1.0/email/send'
+}
+const {
+    SERVICE_ID: serviceID,
+    TEMPLATE_ID: templateID,
+    PUBLIC_KEY: userID,
+    API_URL: url
+} = emailjs_data
+
+/* Datos ingresados en el formulario */
+let firstName = document.getElementById('name');
+let last = document.getElementById('last_name');
+let email = document.getElementById('email');
+let reason = document.getElementById('reason');
+let message = document.getElementById('message');
+
+const form = document.getElementById('form'); /* captura del formulario */
+/* Añadimos el evento submit */
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (firstName.value == "" || last.value == "" || email.value == "" || message.value == "") {
+        alert("Complete los campos");
+    } else {
+        sendEmail(firstName.value, last.value, email.value, message.value);
+    }  
+})
+
+const sendEmail = (name, last, email, message) => {
+    const sendData = {
+        service_id: serviceID,
+        template_id: templateID,
+        user_id: userID,
+        template_params: {
+            name: name,
+            last: last,
+            from_email: email,
+            reason: "Motivo de consulta",
+            message: message
+        },
+
+    }
+    fetch(url,{
+        method: 'POST',
+        body: JSON.stringify(sendData),
+        headers: {"Content-type": "application/json;charset=UTF-8"}
+    })
+    .then((response) => response)
+    .then((json) => console.log(json))
+    .catch((error) => console.log(error));
+}
+
+// let select = document.getElementById('reason-select');
+// select.addEventListener('change', () => {
+//     let value = select.options[select.selectedIndex].textContent;
+//     console.log(value);
+// });
